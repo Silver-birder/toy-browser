473edcc947aeeb078b2eaf45371744398ce1815a
shibamoto <silverbirder@gmail.com>
2021-05-09 21:36:00 +0900

    copy original source code (src)

---
+++ src2/parse/html.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+namespace html_parser {
+  std::string parseHtml(const std::string &source) {
+    return "aaa";
+  };
+}
---
+++ src2/parse/html.h
@@ -0,0 +1,7 @@
+#ifndef HTML_PARSER_H
+#define HTML_PARSER_H
+
+namespace html_parser {
+  std::string parseHtml(const std::string &source);
+}
+#endif
