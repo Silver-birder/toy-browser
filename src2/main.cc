db6a1fa7b9f604a9df0827af1abfbe52d75d5572
shibamoto <silverbirder@gmail.com>
2021-05-16 00:01:11 +0900

    add code to main

--- src2/main.cc
+++ src2/main.cc
@@ -5,6 +5,8 @@
 #include "util.h"
 #include "parse/css.h"
 #include "parse/html.h"
+#include "render/text.h"
+#include "style.h"
 
 DEFINE_string(html_file, "examples/demo.html", "HTML file to load");
 DEFINE_string(css_file, "examples/demo.css", "CSS file to load");
@@ -16,5 +18,8 @@ int main(int argc, char **argv) {
   const std::string css = io::readFile(FLAGS_css_file);
   std::unique_ptr<dom::Node> root = html_parser::parseHtml(source);
   const std::unique_ptr<css::StyleSheet const> stylesheet = css::parseCss(css);
+
+  text_render::FontRegistry *registry =　text_render::FontRegistry::getInstance();
+  std::unique_ptr<style::StyledNode> styled_node = style::styleTree(*root, stylesheet, style::PropertyMap());
   return 0;
 }
